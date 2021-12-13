416497c15af2bd45658ce32a44e3cc4036ca519c
ChandrashekharM3018 <62007551+ChandrashekharM3018@users.noreply.github.com>
2021-12-14 01:11:29 +0530

    Update intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-17 mb-17 md:mb-13">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+      <h4 className="text-center md:text-left text-lg mt-7 md:pl-10">
+        An amazing generated blog example using{' '}
         <a
           href="https://www.gatsbyjs.com/"
           className="underline hover:text-success duration-200 transition-colors"
