0f9a188ba41745a8c835f76f3bf4afdb3824c78b
ChandrashekharM3018 <62007551+ChandrashekharM3018@users.noreply.github.com>
2021-12-14 01:10:20 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 
 export default function Header() {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <h2 className="text-2xl md:text-6xl font-bold tracking-tight md:tracking-tighter leading-tight mb-25 mt-10">
       <Link to="/" className="hover:underline">
         Blog
       </Link>
